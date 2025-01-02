62973d66614c368fc195ed24e069cc4f820f29d3
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-01 20:11:43 -0500

    Complete JS exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedString = "";
+    for (i = stringArray.length - 1; i >= 0; i--){
+        reversedString += stringArray[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
