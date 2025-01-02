5232a199d7322e94c8748d014e39c33504aee055
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-01 21:07:49 -0500

    Complete JS exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    let sum = 0;
+    let smallNum, largeNum;
+    if (num1 < num2){
+        smallNum = num1;
+        largeNum = num2;
+    } else {
+        smallNum = num2;
+        largeNum = num1;
+    }
 
+    for (i = smallNum; i <= largeNum; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
