163a1e3179e1981d3c861803b4b9d49c7883be5c
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-01 20:58:38 -0500

    Complete JS exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = [];
+    for (const value of array) {
+        if (!remove.includes(value)){
+             newArray.push(value);
+         }
+    }   
+    return newArray;
+
 };
 
 // Do not edit below this line
