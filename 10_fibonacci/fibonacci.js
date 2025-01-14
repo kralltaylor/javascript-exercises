7d8af5e8a6f2a62b0b009746a000d6e04279d30e
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-13 22:05:22 -0500

    Complete JS exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibCurr = 0;
+    let fibNext = 1;
+    let fibTemp = 0;
+    if (num < 0) return "OOPS";
 
+    for (i = num; i > 0; i--){
+        fibTemp = fibCurr + fibNext;
+        fibCurr = fibNext;
+        fibNext = fibTemp;
+    }
+    return fibCurr;
 };
 
 // Do not edit below this line
