0cdaacdf5cbd523eb271d15624deb828e55c5337
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-13 20:28:14 -0500

    Complete JS exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+	const sum = arr.reduce((total, curr) => {
+    return total + curr;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+	const product = arr.reduce((total, curr) => {
+    return total * curr;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let factorial = 1;
+	for (i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
