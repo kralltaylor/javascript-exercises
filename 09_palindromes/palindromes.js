c5281d55403ed0ee8d7df779798c87e18bb48655
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-13 20:48:05 -0500

    Complete JS exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "");
+    str = str.toLowerCase();
+    let str2 = '';
+    for (i = str.length; i >= 0; i--){
+        str2 = str2.concat('', str.charAt(i));
+    }
+    if (str === str2){
+        return true;
+    } else return false;
+}
 // Do not edit below this line
 module.exports = palindromes;
